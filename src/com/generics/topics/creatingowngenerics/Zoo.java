f44b31e8a22baf3e2a739a934f402cba2038f05c
utsavkc24 <utsavkc24@gmail.com>
2024-07-05 00:24:25 +0530

    Generics with constructors

--- src/com/generics/topics/creatingowngenerics/Zoo.java
+++ src/com/generics/topics/creatingowngenerics/Zoo.java
@@ -13,5 +13,13 @@ public class Zoo {
         //THESE WILL COMPILATION ERROR
 //        monkeyCage.setAnimal(new Lion());
 //        lionCage.setAnimal(new Monkey());
+
+
+        //Below line, compiler doesn't perform type safety check
+//        Cage<Monkey> anotherMonkeyCage = new Cage(new Monkey(), new Lion());
+        //So, while using constructors with generic, we should add '<>' before const
+        // args, to tell compiler to perform type safety check and hence it will compilation error
+//        Cage<Monkey> anotherMonkeyCageWithCheck = new Cage<>(new Monkey(), new Lion());
+
     }
 }
