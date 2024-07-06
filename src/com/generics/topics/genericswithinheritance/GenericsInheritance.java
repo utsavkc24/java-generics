533467108dcf79a79ff29afeca558e5708472dcb
utsavkc24 <utsavkc24@gmail.com>
2024-07-07 00:23:29 +0530

    What is a wildcard?

--- src/com/generics/topics/genericswithinheritance/GenericsInheritance.java
+++ src/com/generics/topics/genericswithinheritance/GenericsInheritance.java
@@ -8,22 +8,26 @@ import java.util.List;
 // for eg - below code will give the compilation errors,
 // as we are passing Integer & String both to Object type to print the list
 // Reasons, as you can type safety doesn't as we can any child of objects(see printList() methods)
+//Solution - we can use wildcards(?), that's says send anything, any type
+//now below code will not give CE
+//What is wildcards, The question mark (?) is known as the wildcard in generic programming. It represents an unknown //type. The wildcard can be used in a variety of situations such as the type of a parameter, field, or local //variable; sometimes as a return type
 public class GenericsInheritance {
     public static void main(String[] args) {
         List<String> strings = new ArrayList<>();
         strings.add("Bruce");
         strings.add("Wayne");
-//        printList(strings); COMPILATION ERRORS
+        printList(strings);
 
         List<Integer> integers = new ArrayList<>();
         integers.add(2);
         integers.add(4);
-//        printList(integers); COMPILATION ERRORS
+        printList(integers);
     }
 
-    private static void printList(List<Object> objectsList) {
+    private static void printList(List<?> objectsList) {
         objectsList.forEach(System.out::println);
-        objectsList.add("NO");
-        objectsList.add(1);
+        //But still by using wildcards, you can read(print) a list but cannot write to that list, hence below line will give compilation errors
+//        objectsList.add("NO");
+//        objectsList.add(1);
     }
 }
