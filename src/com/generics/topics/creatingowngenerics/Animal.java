13831c35e6a27e26d57fba26e9556e395654f221
utsavkc24 <utsavkc24@gmail.com>
2024-07-08 19:49:04 +0530

    Using generics with static methods

--- src/com/generics/topics/creatingowngenerics/Animal.java
+++ src/com/generics/topics/creatingowngenerics/Animal.java
@@ -1,6 +1,6 @@
 package com.generics.topics.creatingowngenerics;
 
-public class Animal implements Diet, Habitat{
+public class Animal implements Diet, Habitat {
     @Override
     public void diet() {
 
@@ -9,5 +9,15 @@ public class Animal implements Diet, Habitat{
     @Override
     public void habitat() {
 
+    }
+
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
--- src/com/generics/topics/creatingowngenerics/Cage.java
+++ src/com/generics/topics/creatingowngenerics/Cage.java
@@ -38,4 +38,15 @@ public class Cage<E extends Animal & Diet & Habitat> {
         animal.habitat();
         anotherAnimal.habitat();
     }
+    //below code will give error saying -> 'com.generics.topics.creatingowngenerics.Cage.this' cannot be referenced from a static context
+    //As ref instance variable 'E' in a static context & this E(not in above class def public class Cage<E extends Animal & Diet & Habitat> {) in below method
+    //signature does'nt have type of the E(does'nt what is the type of 'E'), so we must explicitly tell in method sign, see after this method
+//    public static boolean isCompatible(E animal, E anotherAnimal){
+//        return animal.getType().equals(anotherAnimal.getType());
+//    }
+
+    // we are extending Animal also in method sign(<E extends Animal>) as we want to use getType()
+    public static <E extends Animal> boolean isCompatible(E animal, E anotherAnimal) {
+        return animal.getType().equals(anotherAnimal.getType());
+    }
 }
